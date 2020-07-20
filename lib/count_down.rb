ed5bd62cb5a6f93f80de5e3392c18cd342dc7606
Garret Gallant <gallanttechnological@gmail.com>
2020-07-20 17:26:14 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,8 @@
 # Write your code here
 
-while true do
-  puts "say this forever..."
-end
+# while true do
+#   puts "say this forever..."
+# end
 
 count = 0 
 
@@ -10,6 +10,8 @@ while count < 3 do
   puts "I am the #{count}, I love to count!"
   ++count
 end
+
+
 
 
 
