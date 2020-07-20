5209ef18717b87cf0412021009ff38d0f290bbb4
Garret Gallant <gallanttechnological@gmail.com>
2020-07-20 17:33:39 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,28 @@
 # Write your code here
+
+# # while true do
+# #   puts "say this forever..."
+# # end
+
+# term_var = 7
+# count = 0 
+
+# while count < 10 do
+#   break if count == term_var
+#   puts "I am the #{count}, I love to count!"
+#   count = count + 1
+# end
+
+count = 10 
+
+while count >= 1 do
+  puts count
+  count -= 1
+end
+
+puts "Happy New Year!"
+
+
+
+
+
