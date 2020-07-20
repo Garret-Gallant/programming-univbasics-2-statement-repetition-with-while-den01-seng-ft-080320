58dd5a4626a06eeca7fe459eca6e24535aba247d
Garret Gallant <gallanttechnological@gmail.com>
2020-07-20 17:27:35 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,11 +4,13 @@
 #   puts "say this forever..."
 # end
 
+term_var = 7
 count = 0 
 
-while count < 3 do 
+while count < 10 do
+  break if count == term_var
   puts "I am the #{count}, I love to count!"
-  ++count
+  count = count + 1
 end
 
 
