9450acda558241bda8be3eac379b62133b111b30
Garret Gallant <gallanttechnological@gmail.com>
2020-07-20 17:28:02 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,17 +1,17 @@
 # Write your code here
 
-# while true do
-#   puts "say this forever..."
+# # while true do
+# #   puts "say this forever..."
+# # end
+
+# term_var = 7
+# count = 0 
+
+# while count < 10 do
+#   break if count == term_var
+#   puts "I am the #{count}, I love to count!"
+#   count = count + 1
 # end
-
-term_var = 7
-count = 0 
-
-while count < 10 do
-  break if count == term_var
-  puts "I am the #{count}, I love to count!"
-  count = count + 1
-end
 
 
 
